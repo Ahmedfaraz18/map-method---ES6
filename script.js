803f5ce6c38fbb0bc17b51df4bed7b2dc0c8557c
Ahmed faraz <127437190+Ahmedfaraz18@users.noreply.github.com>
2024-03-10 15:42:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let data = [
+    {
+    name : "Ahmed faraz",
+    university : "GCU Hyderabad", 
+    Department : "BS IT (information technology)"
+}
+
+{
+    name : "Abdul Aziz",
+    university : "GCU Hyderabad", 
+    Department : "BS DS (Data Science)"
+}
+
+{
+    name : "Sakhawat",
+    university : "GCU Hyderabad", 
+    Department : "BS AI (Artifical intelegence)"
+}
+]
+
+data.map((val,ind)=>{
+    
+})
